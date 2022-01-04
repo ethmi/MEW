ethmi/MEW

commit 499bb6b554ffea14e958d00e55fe83e5e147aec5

chore: 👷 make sure errors still show

src/modules/access-wallet/common/WalletErrorHandler.js
@@ -11,7 +11,12 @@ const WalletErrorHandler = (errors, warnings) => {
   const warningValues = Object.keys(warnings);
   return err => {
     const foundError = errorValues.find(item => {
-      const message = err.message?.message || err.message;
+      const message =
+        err && err.message
+          ? err.message.hasOwProperty('message')
+            ? err.message.message
+            : err.message
+          : err;
       if (!message) return false;
       return message.includes(item);
     });

commit 97eeef7fb6092df9bd7bf2dbcb9f5564344690e7

fix: 🐛restore display of error messages
Regression from 499bb6b554ffea14e958d00e55fe83e5e147aec5. Error
messages were not shown, instead a an unhandled exception was shown
that 'hasOwProperty' is not a function.

src/modules/access-wallet/common/WalletErrorHandler.js
@@ -13,7 +13,7 @@ const WalletErrorHandler = (errors, warnings) => {
     const foundError = errorValues.find(item => {
       const message =
         err && err.message
-          ? err.message.hasOwProperty('message')
+          ? err.message.hasOwnProperty('message')
             ? err.message.message
             : err.message
           : err;
@@ -21,7 +21,14 @@ const WalletErrorHandler = (errors, warnings) => {
       return message.includes(item);
     });
     const foundWarning = warningValues.find(item => {
-      return err.message.includes(item);
+      const message =
+        err && err.message
+          ? err.message.hasOwnProperty('message')
+            ? err.message.message
+            : err.message
+          : err;
+      if (!message) return false;
+      return message.includes(item);
     });
     if (foundError) {
       Toast(Vue.$i18n.t(errors[foundError]), {}, ERROR);